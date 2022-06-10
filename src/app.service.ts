b103af27f9ecb948602e63c71aafafba998947ec
Nourankh20 <82681541+Nourankh20@users.noreply.github.com>
2022-06-10 17:42:18 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -15,26 +15,23 @@ type item = {
 
 @Injectable()
 export class AppService {
-  
   params = {
     QueueUrl: process.env.PRODUCT_SQS_K,
   };
   private sqs;
   private sns;
-  
+
   constructor(
     @InjectModel(Inventory.name)
-    private inventroyModel: Model<InventoryDocument>,) {
-
-      AWS.config.update({
-        region: 'us-east-1',
-        accessKeyId: process.env.ACCESS_KEY_ID_K,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY_K,
-      });
-      this.sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
-      this.sns = new AWS.SNS({ apiVersion: '2010-03-31' });
-
-
+    private inventroyModel: Model<InventoryDocument>,
+  ) {
+    AWS.config.update({
+      region: 'us-east-1',
+      accessKeyId: process.env.ACCESS_KEY_ID_K,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY_K,
+    });
+    this.sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
+    this.sns = new AWS.SNS({ apiVersion: '2010-03-31' });
   }
 
   getHello(): string {
@@ -45,75 +42,69 @@ export class AppService {
     return await this.inventroyModel.find().exec();
   }
 
-
-  async getProduct(id : string) {
-    return await this.inventroyModel.findOne({_id:id}).exec();
+  async getProduct(id: string) {
+    return await this.inventroyModel.findOne({ _id: id }).exec();
   }
 
+  async buy(body: any): Promise<any> {
+    const z = await body;
+    const msg = {
+      id: z.item,
+      stock: z.quantity,
+    };
+    let prod = await this.inventroyModel.findOne({ _id: msg.id }).exec();
+    console.log('prod', prod.stock);
+    let newQuantity;
 
-
-
-  async buy(body: any):Promise<any>{
-      const z = await body;
-      const msg = {
-        id: z.item,
-        stock:z.quantity
-      }
-      let prod = await this.inventroyModel.findOne({_id:msg.id}).exec(); 
-      console.log('prod', prod.stock)
-      let newQuantity;
-
-      if(prod.stock>msg.stock){
-        newQuantity = prod.stock - msg.stock
-            }
-      else{
-        newQuantity = 0
-      }
-      await this.inventroyModel.updateOne({_id:msg.id},
+    if (prod.stock > msg.stock) {
+      newQuantity = prod.stock - msg.stock;
+    } else {
+      newQuantity = 0;
+    }
+    await this.inventroyModel
+      .updateOne(
+        { _id: msg.id },
         {
-        $set: {
-          stock: newQuantity,
+          $set: {
+            stock: newQuantity,
+          },
         },
-      },
-      { upsert: true }).exec();
-    
+        { upsert: true },
+      )
+      .exec();
 
-      prod = await this.inventroyModel.findOne({_id:msg.id}).exec(); 
-      console.log('prod', prod.stock)
+    prod = await this.inventroyModel.findOne({ _id: msg.id }).exec();
+    console.log('prod', prod.stock);
+  }
 
-   }
-
-
-   consume() {
-    console.log("zewwwww products") 
-    console.log('process.env.PRODUCT_SQS_K', process.env.PRODUCT_SQS_K)
+  consume() {
+    console.log('zewwwww products');
+    console.log('process.env.PRODUCT_SQS_K', process.env.PRODUCT_SQS_K);
     Consumer.create({
       queueUrl: process.env.PRODUCT_SQS_K,
       handleMessage: async (message) => {
-
         var params = {
-          Entries: [  //required 
+          Entries: [
+            //required
             {
               Id: message.MessageId, //required
-              ReceiptHandle: message.ReceiptHandle //required
+              ReceiptHandle: message.ReceiptHandle, //required
             },
-            // more items 
+            // more items
           ],
-          QueueUrl: process.env.PRODUCT_SQS_K //required 
+          QueueUrl: process.env.PRODUCT_SQS_K, //required
         };
 
         var x = await JSON.parse(message.Body);
         var y = await JSON.parse(x.Message);
-        console.log(y)
-        await this.buy(y)
-        this.sqs.deleteMessageBatch(params, function(err, data) {
-          if (err) console.log(err, err.stack); // an error occurred
-          else     console.log(data);           // successful response
+        console.log(y);
+        await this.buy(y).then(() => {
+          this.sqs.deleteMessageBatch(params, function (err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else console.log(data); // successful response
+          });
         });
       },
-    }).start()
-
+    }).start();
   }
- 
-
 }
