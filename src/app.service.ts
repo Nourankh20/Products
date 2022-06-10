0d2883e1d6a003c529c15321e40eb85f68361ac5
Nourankh20 <82681541+Nourankh20@users.noreply.github.com>
2022-06-10 17:38:09 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -105,7 +105,7 @@ export class AppService {
         var x = await JSON.parse(message.Body);
         var y = await JSON.parse(x.Message);
         console.log(y)
-        this.buy(y)
+        await this.buy(y)
         this.sqs.deleteMessageBatch(params, function(err, data) {
           if (err) console.log(err, err.stack); // an error occurred
           else     console.log(data);           // successful response
